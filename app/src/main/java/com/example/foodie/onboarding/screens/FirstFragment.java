588cfca7772b2af9de03c25918745bf3871fbedc
Muaz osama <moazosama204@gmail.com>
2026-01-30 06:46:28 +0200

    add on boarding first and second screens' UI and functionality

--- app/src/main/java/com/example/foodie/onboarding/screens/FirstFragment.java
+++ app/src/main/java/com/example/foodie/onboarding/screens/FirstFragment.java
@@ -3,10 +3,12 @@ package com.example.foodie.onboarding.screens;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.foodie.R;
 
@@ -21,6 +23,15 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view = inflater.inflate(R.layout.fragment_first, container, false);
+        Button next = view.findViewById(R.id.onboarding_one_next_btn);
+        ViewPager2 viewPager = getActivity().findViewById(R.id.view_pager);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPager.setCurrentItem(1);
+            }
+        });
+        return view;
     }
 }
--- app/src/main/java/com/example/foodie/onboarding/screens/SecondFragment.java
+++ app/src/main/java/com/example/foodie/onboarding/screens/SecondFragment.java
@@ -1,14 +1,18 @@
 package com.example.foodie.onboarding.screens;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.foodie.R;
+import com.example.foodie.home.HomeActivity;
 
 public class SecondFragment extends Fragment {
 
@@ -21,6 +25,18 @@ public class SecondFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        Button next = view.findViewById(R.id.onboarding_second_next_btn);
+        ViewPager2 viewPager = getActivity().findViewById(R.id.view_pager);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
+
 }
+
