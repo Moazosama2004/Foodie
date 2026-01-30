a6dcef771eaddd5f53eb482bb2d39fe8be0755ec
Muaz osama <moazosama204@gmail.com>
2026-01-30 11:20:10 +0200

    add signup screen UI and basic login navigation, the login and signup layouts are also wrapped in ScrollViews to prevent content from being obscured on smaller screens.

--- app/src/main/java/com/example/foodie/auth/LoginFragment.java
+++ app/src/main/java/com/example/foodie/auth/LoginFragment.java
@@ -1,16 +1,26 @@
 package com.example.foodie.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.foodie.R;
+import com.example.foodie.home.HomeActivity;
 
 public class LoginFragment extends Fragment {
+
+    private Button btnLogin;
+    private TextView txtRegister;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -21,5 +31,27 @@ public class LoginFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        btnLogin =  view.findViewById(R.id.btn_login);
+        txtRegister = view.findViewById(R.id.txt_register);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity() , HomeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(v).navigate(R.id.action_loginFragment_to_signupFragment);
+            }
+        });
     }
 }
